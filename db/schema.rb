df4a299f12fc7748c07d5ab98a69cb1b5ded917c
Cosmin Tuhut <cosmintuhut@gmail.com>
2018-09-26 15:43:01 +0100

    Linked posts to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_104540) do
+ActiveRecord::Schema.define(version: 2018_09_26_144026) do
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2018_09_26_104540) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
